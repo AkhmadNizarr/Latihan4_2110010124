e7f1a13666029a7cd29f20626b537454f90035b6
AkhmadNizarr <nizarbjb87@gmail.com>
2023-06-13 08:53:58 +0700

    membuat program latihan 4

--- src/penilaian/NilaiAkhir.java
+++ src/penilaian/NilaiAkhir.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package penilaian;
 
-/**
- *
- * @author Lenovo
- */
-public class NilaiAkhir {
+public class NilaiAkhir extends Mahasiswa {
+     int uts, uas, tugas;
+    
+    
+    public NilaiAkhir(String nama, String npm, int uts, int uas, int tugas) {
+        super.nama = nama;
+        super.npm = npm;
+        this.uts = uts;
+        this.uas = uas;
+        this.tugas = tugas;
+    
+   
+    }
+
+    @Override
+    public void setNpm(String npm) {
+        super.setNpm(npm); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setNama(String nama) {
+        super.setNama(nama); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNpm() {
+        return super.getNpm(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNama() {
+        return super.getNama(); //To change body of generated methods, choose Tools | Templates.
+    }
+    public int hitungNilai akhir(){
+        return (int)(0.3*uts + 0.3*uas + 0.4*tugas)
+    }
     
 }
